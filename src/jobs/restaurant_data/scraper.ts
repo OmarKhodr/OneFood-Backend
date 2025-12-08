15acc2b5134c402ecae2e3d20decc0f02c5ee41a
Omar Khodr <omar.khoder2000@gmail.com>
2025-12-08 23:05:50 +0100

    Add placeholder functions for fetching static restaurant data

--- src/jobs/restaurant_data/scraper.ts
+++ src/jobs/restaurant_data/scraper.ts
@@ -1,15 +1,46 @@
 import "axios";
+import { err, ok, Result, unwrapOrThrow } from "#utils/result.js";
+import fs from "fs";
+import { homedir } from "os";
+import path from "path";
+
+const DELIVEROO_BASE_URL = "https://deliveroo.ae";
+
+async function fetchRobotsTxt(url: string): Promise<Result<string>> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    return err(`Failed to fetch ${url}: ${response.statusText}`);
+  }
+  const text = await response.text();
+  return ok(text);
+}
+
+interface Restaurant {
+  title: string;
+}
+
+async function fetchDeliverooRestaurantData(): Promise<Result<Restaurant[]>> {
+  const url = `${DELIVEROO_BASE_URL}/restaurants/dubai/dubai-business-bay?geohash=thrr3squys6w&collection=all-restaurants`;
+  const allRestaurantsResponse = await fetch(url);
+  if (!allRestaurantsResponse.ok) {
+    return err(`Failed to fetch ${url}: ${allRestaurantsResponse.statusText}`);
+  }
+  console.log("Fetched restaurant HTML data");
+  const content = await allRestaurantsResponse.text();
+  const filePath = path.join(homedir(), "Downloads", "restaurants.txt");
+
+  fs.writeFileSync(filePath, content, "utf8");
+  console.log(`Restaurant data written to ${filePath}`);
+  return ok(Array<Restaurant>());
+}
 
 // TODO: Pass database instance when inserting scraped data
 export async function scrapeDelierooData() {
   console.log("Scraper job started");
   // Fetch robots.txt file from deliveroo.ae
-  try {
-    const response = await fetch("https://deliveroo.ae/robots.txt");
-    const data = await response.text();
-    console.log("Fetched robots.txt content:");
-    console.log(data);
-  } catch (error) {
-    console.error("Error fetching robots.txt:", error);
-  }
+  const robotsTxt = unwrapOrThrow(
+    await fetchRobotsTxt(`${DELIVEROO_BASE_URL}/robots.txt`),
+  );
+  console.log("Fetched robots.txt:");
+  console.log(robotsTxt);
 }
